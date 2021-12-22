17cf14dc2da599a5d526bac9b2de5443d2e494f0
ivasys <ivasys@gmail.com>
2021-12-22 09:50:36 -0500

    Update server.js

--- server.js
+++ server.js
@@ -9,15 +9,4 @@ server.use(router);
 
 server.listen(port);
 
-var express = require('express'),
-   app = express(),
-   port = process.env.PORT || 8080;
 
-app.listen(port);
-app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    next();
-});
